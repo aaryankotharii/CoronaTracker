52909f20be90f29c73150468c7948492ab3c2331
aaryankotharii <aaryan.kothari@gmail.com>
2020-05-17 17:36:43 +0530

    CountriesData.json added

--- CoronaTracker/Model/CoronaClient.swift
+++ CoronaTracker/Model/CoronaClient.swift
@@ -75,5 +75,4 @@ class CoronaClient {
             completion([],error)
         }
     }
-    
 }
--- CoronaTracker/Model/Country.swift
+++ CoronaTracker/Model/Country.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 struct CountryData : Codable{
     let countries : [Country]
@@ -17,3 +18,28 @@ struct Country : Codable{
     let Slug : String
     let ISO2 : String
 }
+
+
+//let landmarkData: [Landmark] = load("landmarkData.json")
+//
+//func load<T: Decodable>(_ filename: String) -> T {
+//    let data: Data
+//
+//    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+//        else {
+//            fatalError("Couldn't find \(filename) in main bundle.")
+//    }
+//
+//    do {
+//        data = try Data(contentsOf: file)
+//    } catch {
+//        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+//    }
+//
+//    do {
+//        let decoder = JSONDecoder()
+//        return try decoder.decode(T.self, from: data)
+//    } catch {
+//        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+//    }
+//}
