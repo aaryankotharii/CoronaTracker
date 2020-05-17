97be8f59c538af73e2edfae1c7e07b47d7844b81
aaryankotharii <aaryan.kothari@gmail.com>
2020-05-17 15:00:07 +0530

    country struct added

---
+++ CoronaTracker/Model/Country.swift
@@ -0,0 +1,15 @@
+//
+//  Country.swift
+//  CoronaTracker
+//
+//  Created by Aaryan Kothari on 17/05/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import Foundation
+
+struct Country {
+    let Country : String
+    let Slug : String
+    let ISO2 : String
+}
