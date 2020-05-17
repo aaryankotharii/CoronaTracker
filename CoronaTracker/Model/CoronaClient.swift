a4f4e1cede2093eb7b22be7b43a63dec82f577d7
aaryankotharii <aaryan.kothari@gmail.com>
2020-05-17 14:48:33 +0530

    CoronaClient added

---
+++ CoronaTracker/Model/CoronaClient.swift
@@ -0,0 +1,15 @@
+//
+//  CoronaClient.swift
+//  CoronaTracker
+//
+//  Created by Aaryan Kothari on 17/05/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import Foundation
+
+class CoronaClient {
+    
+    
+    
+}
