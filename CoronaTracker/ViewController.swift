2cf671671d5a2bc3560eb0f3ef73f0662bff8810
aaryankotharii <aaryan.kothari@gmail.com>
2020-05-17 14:46:59 +0530

    initial commit

---
+++ CoronaTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoronaTracker
+//
+//  Created by Aaryan Kothari on 17/05/20.
+//  Copyright © 2020 Aaryan Kothari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
