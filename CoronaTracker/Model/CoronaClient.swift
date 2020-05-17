b218951e50ae274115a96b271bad3c5e59586be0
aaryankotharii <aaryan.kothari@gmail.com>
2020-05-17 15:00:51 +0530

    taskForGETRequest added

--- CoronaTracker/Model/CoronaClient.swift
+++ CoronaTracker/Model/CoronaClient.swift
@@ -41,4 +41,38 @@ class CoronaClient {
         }
     }
     
+    
+    //MARK:- GET REQUEST
+    class func taskForGETRequest<ResponseType: Decodable>(url: URL, responseType: ResponseType.Type, completion: @escaping (ResponseType?, Error?) -> Void) -> URLSessionDataTask {
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data else {
+                DispatchQueue.main.async {
+                    completion(nil, error)
+                }
+                return
+            }
+            let decoder = JSONDecoder()
+            do {
+                let responseObject = try decoder.decode(ResponseType.self, from: data)
+                DispatchQueue.main.async {
+                    completion(responseObject, nil)
+                }
+            } catch {
+                do {
+                    let errorResponse = try decoder.decode(ErrorResponse.self, from: data) as Error
+                    DispatchQueue.main.async {
+                        completion(nil, errorResponse)
+                    }
+                } catch {
+                    DispatchQueue.main.async {
+                        completion(nil, error)
+                    }
+                }
+            }
+        }
+        task.resume()
+        
+        return task
+    }
+    
 }
