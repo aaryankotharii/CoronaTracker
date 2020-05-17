1a7f23f88db7346a41745fb8c4aef46bc5869c35
aaryankotharii <aaryan.kothari@gmail.com>
2020-05-17 14:58:43 +0530

    Endpoints added

--- CoronaTracker/Model/CoronaClient.swift
+++ CoronaTracker/Model/CoronaClient.swift
@@ -10,6 +10,35 @@ import Foundation
 
 class CoronaClient {
     
-    
+    //MARK:- ENDPOINT URLS
+    enum Endpoints {
+        
+        static let base = "https://api.covid19api.com"
+        
+        case countries
+        case summary
+        case dayone(country:String)
+        case dayonelive(country:String)
+        case dayonetotal(country:String)
+        
+        var stringValue : String{
+            switch self {
+            case .countries:
+                return Endpoints.base + "/countries"
+            case .summary:
+                return Endpoints.base + "/summary"
+            case .dayone(let country):
+                 return Endpoints.base + "/dayone/country/\(country)"
+            case .dayonelive(let country):
+                return Endpoints.base + "/dayone/country/\(country)/status/confirmed/live"
+            case .dayonetotal(let country):
+                return Endpoints.base + "/dayone/country/\(country)/status/confirmed"
+            }
+        }
+        
+        var url : URL {
+            return URL(string: self.stringValue)!
+        }
+    }
     
 }
