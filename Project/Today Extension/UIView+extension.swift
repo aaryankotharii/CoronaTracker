3f946b8c753335baa6703fd623381977edd4f1fa
aaryankotharii <aaryan.kothari@gmail.com>
2020-05-27 20:02:44 +0530

    UIView IBInspectable properties added

--- Project/Today Extension/UIView+extension.swift
+++ Project/Today Extension/UIView+extension.swift
@@ -6,4 +6,45 @@
 //  Copyright © 2020 Aaryan Kothari. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+//MARK:- Adding Attributes to UIView
+extension UIView {
+
+@IBInspectable
+var cornerRadius: CGFloat {
+    get {
+        return layer.cornerRadius
+    }
+    set {
+        layer.cornerRadius = newValue
+    }
+}
+
+@IBInspectable
+var borderWidth: CGFloat {
+    get {
+        return layer.borderWidth
+    }
+    set {
+        layer.borderWidth = newValue
+    }
+}
+
+@IBInspectable
+var borderColor: UIColor? {
+    get {
+        if let color = layer.borderColor {
+            return UIColor(cgColor: color)
+        }
+        return nil
+    }
+    set {
+        if let color = newValue {
+            layer.borderColor = color.cgColor
+        } else {
+            layer.borderColor = nil
+        }
+    }
+}
+}
